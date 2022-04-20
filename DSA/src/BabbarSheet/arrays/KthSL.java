c6ce33042137886580df7a4d2a2a70d2bcb18690
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2022-04-20 07:54:15 +0530

    Sort an array with 0,1,2 my Approach

--- DSA/src/BabbarSheet/arrays/KthSL.java
+++ DSA/src/BabbarSheet/arrays/KthSL.java
@@ -4,17 +4,17 @@ import java.util.*;
 
 public class KthSL {
     public static void main(String[] args) {
-        int arr[] = {7, 10, 4, 3, 20, 15};
+        int[] arr = {7, 10, 4, 3, 20, 15};
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter K th number :");
         int k = sc.nextInt();
-        int kthMin = getKthMin(arr,k);
-        System.out.println(""+ k+" th min is "+ kthMin);
+        int kthMin = myApproach(arr, k);
+        System.out.println("" + k + " th min is " + kthMin);
     }
 
-    private static int myApproach(int []arr, int k){
+    private static int myApproach(int[] arr, int k) {
         Arrays.sort(arr);
-        return arr[k-1];
+        return arr[k - 1];
 
         /**
          * TC -> depends on the sorting algo used
---
+++ DSA/src/BabbarSheet/arrays/Sort012WithoutSortingAlgo.java
@@ -0,0 +1,42 @@
+package BabbarSheet.arrays;
+
+import java.util.Arrays;
+
+public class Sort012WithoutSortingAlgo {
+    public static void main(String[] args){
+         int[] arr ={0,1,2,1,2} ;
+        myApproach(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void myApproach(int a[], int n)
+    {
+        // code here
+        int nextPos = 0;
+        for (int i = nextPos; i<n; i++ ){
+            if(nextPos!= n-1 && a[i] == 0){
+                swap(a,i,nextPos);
+                nextPos++;
+            }
+        }
+
+        for (int i = nextPos; i<n; i++ ){
+            if(nextPos!= n-1 && a[i] == 1){
+                swap(a,i,nextPos);
+                nextPos++;
+            }
+        }
+
+        for (int i = nextPos; i<n; i++ ){
+            if(nextPos!= n-1 && a[i] == 2){
+                swap(a,i,nextPos);
+                nextPos++;
+            }
+        }
+    }
+
+    public static void swap(int a[], int i, int nextPos){
+        int temp = a[nextPos];
+        a[nextPos] = a[i];
+        a[i] = temp;
+    }
+}
