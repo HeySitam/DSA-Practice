2fb3a65f99e928652b4f45351b27da9be2ac04f1
Sitam Sardar <sitamadex11@gmail.com>
2022-06-26 10:33:14 +0530

    Array Reverse recursive approach

--- DSA/src/Arrays/BabbarSheet/ReverseTheArray.java
+++ DSA/src/Arrays/BabbarSheet/ReverseTheArray.java
@@ -6,12 +6,13 @@ public class ReverseTheArray {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {1, 2, 3};
-        myApproach(arr1);
-        approach2(arr2);
+       recursiveApproach(arr1,0);
     }
 
     static void myApproach(int[] arr) {
-        for (int i = 0, j = arr.length - 1; i < (arr.length / 2) + 1; i++, j--) {
+        int arrLength = arr.length;
+        int lastIndex = arrLength - 1;
+        for (int i = 0, j = lastIndex; i < (arrLength / 2) + 1; i++, j--) {
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
@@ -29,5 +30,13 @@ public class ReverseTheArray {
             end--;
         }
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void recursiveApproach(int[] arr, int pos){
+        if(pos == arr.length){
+            return;
+        }
+        recursiveApproach(arr,pos+1);
+        System.out.println(arr[pos]);
     }
 }
