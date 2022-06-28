7c41cf8f5eba35a680a15df59312a2d9d5ae285b
Sitam Sardar <sitamadex11@gmail.com>
2022-06-28 22:51:40 +0530

    Duplicate Finding in Array Brute force added

---
+++ DSA/src/Arrays/BabbarSheet/FindDuplicatesInArray.java
@@ -0,0 +1,35 @@
+package Arrays.BabbarSheet;
+
+import java.util.*;
+
+public class FindDuplicatesInArray {
+    public static void main(String[] args){
+        int[] arr = {3,7,9,0,2,3,5,2};
+        List<Integer> list = new FindDuplicatesInArraySolution().bruteForce(arr,arr.length);
+        System.out.println(list);
+    }
+}
+
+class FindDuplicatesInArraySolution{
+
+    // T.C -> O(n^2 + mlogm) or O(n^2)
+    public  ArrayList<Integer> bruteForce(int arr[], int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<n-1; i++){
+            for(int j = i+1; j<n; j++){
+                if(arr[i] == arr[j]){
+                    if(!list.contains(arr[i]))
+                        list.add(arr[i]);
+                }
+            }
+        }
+
+        if(list.isEmpty()){
+            list.add(-1);
+        }
+        // code here
+        Collections.sort(list);
+
+        return list;
+    }
+}
