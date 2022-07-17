8eeb4a573a5d87cab6528f7f2cc79a0bb214cf3b
Sitam Sardar <sitamadex11@gmail.com>
2022-07-17 08:16:17 +0530

    Remove all repeatative elements from a string

---
+++ DSA/src/Recursion/RemoveRepeatatedElements.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class RemoveRepeatatedElements {
+    public static void main(String[] args){
+        String str = "aabccd";
+        System.out.println(rre(str,str.length(),0));
+        System.out.println(""+str.charAt(0));
+    }
+
+    static String rre(String str,int len, int pos){
+        if(pos == len)
+            return "";
+        String newStr = rre(str,len, pos +1);
+        if(isContain(""+str.charAt(pos),newStr)){
+           return newStr;
+        } else {
+            return str.charAt(pos)+newStr;
+        }
+    }
+
+    static boolean isContain(String chkStr, String oriStr){
+       return  oriStr.contains(chkStr);
+    }
+}
