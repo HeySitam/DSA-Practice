ab095f1247d3ec72652396999c1b50136e91560f
Sitam Sardar <sitam.dir@gmail.com>
2022-08-16 10:06:34 +0530

    Find floor  of an sorted array

---
+++ DSA/src/BinarySearch/AdityaVerma/FloorFind.java
@@ -0,0 +1,23 @@
+package BinarySearch.AdityaVerma;
+
+public class FloorFind {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,8,9,10,10,12,15};
+       System.out.println(getFloor(arr, 9));
+    }
+
+    public static int getFloor(int[] arr, int k){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+            if (arr[mid] == k || (arr[mid] < k && arr[mid+1] > k))
+                return mid;
+            else if (arr[mid] > k)
+                end = mid - 1;
+            else if (arr[mid] < k)
+                start = start + 1;
+        }
+        return -1;
+    }
+}
