3c1e665caf0cdc51d0d405d4f6ecb7cada434d94
Sitam Sardar <sitam.dir@gmail.com>
2023-05-14 18:24:59 +0530

    Stack implementaion with LinkedList

--- DSA/src/Stack/basic_stack_impl.kt
+++ DSA/src/Stack/basic_stack_impl.kt
@@ -1,54 +1,112 @@
 package Stack
 
-fun main(){
-    val stackWithArr = StackWithArr()
+fun main() {
+//    val stackWithArr = StackWithArr()
 //    stackWithArr.push(10)
 //    stackWithArr.printArray()
 //    stackWithArr.push(6)
 //    stackWithArr.printArray()
-    stackWithArr.pop()
-    stackWithArr.printArray()
+//    stackWithArr.pop()
+//    stackWithArr.printArray()
+    val stackWithLL = StackWithLL()
+    stackWithLL.push(99)
+    stackWithLL.printStack()
+    stackWithLL.push(32)
+    stackWithLL.printStack()
+    stackWithLL.pop()
+    stackWithLL.printStack()
+    stackWithLL.pop()
+    stackWithLL.printStack()
+    stackWithLL.pop()
+    stackWithLL.printStack()
+}
+
+/**
+ * Stack
+ */
+interface Stack {
+    fun push(item: Int)
+    fun pop(): Int?
 }
 
 /**
  * With Array Impl
  */
-class StackWithArr {
-    private val arr = IntArray(8){_ ->
+class StackWithArr : Stack {
+    private val arr = IntArray(8) { _ ->
         0
     }
     private var top: Int = -1
 
-    fun push(item: Int){
-        if(top == arr.size)
+    override fun push(item: Int) {
+        if (top == arr.size)
             println("Stack Overflow")
-        else{
+        else {
             top++
             arr[top] = item
             println("$item pushed")
         }
     }
 
-    fun pop():Int?{
-        if(top == -1) {
+    override fun pop(): Int? {
+        return if (top == -1) {
             println("Array is empty")
-            return null
-        } else{
+            null
+        } else {
             val popedItem = arr[top]
             arr[top] = 0
-            top --
+            top--
             println("$popedItem poped")
-            return popedItem
+            popedItem
         }
     }
 
-    fun printArray(){
+    fun printArray() {
         println(arr.toList().toString())
     }
 }
 
-
-
 /**
  * With LinkedList Impl
  */
+class Node(var info: Int, var next: Node? = null)
+
+class StackWithLL : Stack {
+    private var top: Node? = null
+    override fun push(item: Int) {
+        val newNode = Node(info = item)
+        if (top == null) {
+            top = newNode
+        } else {
+            val temp = top
+            top = newNode
+            top!!.next = temp
+        }
+    }
+
+    override fun pop(): Int? {
+        return if (top == null) {
+            println("Stack is Empty")
+            null
+        } else {
+            val temp = top
+            top = temp!!.next
+            temp.next = null
+            temp.info
+        }
+    }
+
+    fun printStack() {
+        if (top == null)
+            println("top -> null")
+        else {
+            print("top -> ${top!!.info}")
+            var temp = top!!.next
+            while (temp != null) {
+                print(" -> ${temp.info}")
+                temp = temp.next
+            }
+            println()
+        }
+    }
+}
